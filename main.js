7df2c9ad1e726ece17f714e97be2d4eebc6da1df
Justin Radatti <jdradatti@gmail.com>
2022-04-25 12:21:18 -0700

    spinning cube

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 /*
 	Initializes scene and player movement
 */
+var renderer, scene, camera, mesh;
 
 // initialize scene
 function main() {
 
-	const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 	camera.position.z = 1;
 
-	const scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
 	const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
 	const material = new THREE.MeshNormalMaterial();
 
-	const mesh = new THREE.Mesh( geometry, material );
+	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
-	const renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.render( scene, camera );
 	document.body.appendChild( renderer.domElement );
 
+	animate();
+
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
 }
 
 function playerMovement() {
