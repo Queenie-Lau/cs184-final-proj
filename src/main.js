0acef347e3326697f5d50ee482314a86c34e6595
Dakshina Palasamudrum <dakshinapalas@Dakshinas-MacBook-Pro.local>
2022-05-04 20:27:49 -0700

    modified scene

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { Movement} from './js/movement/FirstPersonMovement.js';
 var renderer, scene, camera, movement, skybox, skyboxGeo, floorTexture, pipeTexture, clock, mixer, coinsGroup; 
 
 var player = {height: 1.8, speed: 0.3, turnSpeed: Math.PI * 0.02};
-var platform = {width: 15, height: 50};
+var platform = {width: 40, height: 40};
 //var velocity = new THREE.Vector3();
 
 clock = new THREE.Clock();
@@ -117,31 +117,46 @@ function initIsland() {
 
 // Instantiate player obstacles
 function initObjects() {
-	initTree(-3, -6, 2.5, 6);
-	initTree(5, 5, 1, 8);
-	initTree(3, 3);
-	initTree(-4, 8);
-	initTree(8, 8);
-	initTree(-6, 9, 1.5, 4, 7);
-	initTree(-7.5, -7.5, 2, 6, 7);
-	initCapsuleTree(.5, .5, -4, 0.4, 1);
+	initTree(-13, -6, 2.5, 6);
+   	initTree(5, 5, 1, 8);
+   	initTree(3, 3);
+   	initTree(-16, 8);
+   	initTree(16, 16);
+   	initTree(-6, 9, 1.5, 4, 7);
+   	initTree(-7.5, -7.5, 2, 6, 7);
+   	initTree(10, 1);
+   	initTree(14, 14);
+   	initTree(10, -10);
+
+
+	initCapsuleTree(.5, .5, -9, 0.4, 1);
 	initCapsuleTree(1, 1, 7, 0.4, 1);
+	initCapsuleTree(.1, .1, 30, 30, 30);
 
-	initBricks(6, .5, -4, .7, .7, .7, brown);
-	initBricks(4, 5, -10, .7, .7, .7, brown);
-	initBricks(-4, 1, 0, .7, .7, .7, brown);
+	initBricks(3.7, 4, 15, .7, .7, .7, brown);
+   	initBricks(4.4, 4, 15, .7, .7, .7, brown);
+   	initBricks(5.1, 4, 15, .7, .7, .7, brown);
+   	initBricks(6, .5, -4, .7, .7, .7, brown);
+   	initBricks(4, 5, -10, .7, .7, .7, brown);
+   	initBricks(-4, 1, 0, .7, .7, .7, brown);
 
-	initPowerUpBox(4, 3, 5, .7, .7, .7);
-	initPowerUpBox(10, 3, 5, .7, .7, .7);
-	initPowerUpBox(-4, 3, 5, .7, .7, .7);
+	initPowerUpBox(14, 3, 5, .7, .7, .7);
+   	initPowerUpBox(10, 3, 5, .7, .7, .7);
+   	initPowerUpBox(-14, 3, 5, .7, .7, .7);
+   	initPowerUpBox(3, 4, 15, .7, .7, .7);
 	
-	initCoin(-18, 2, 0, .3, .3, .1, 32, 1, false);
-	initCoin(-5, 2, 4, .3, .3, .1, 32, 1, false);
-	initCoin(3, 2, 4, .3, .3, .1, 32, 1, false);
+	initCoin(-18, 5, 0, .3, .3, .1, 32, 1, false);
+	initCoin(-5, 5, 4, .3, .3, .1, 32, 1, false);
+	initCoin(3, 5, 4, .3, .3, .1, 32, 1, false);
+	initCoin(1, 2, 15, .3, .3, .1, 32, 1, false);
+	initCoin(1.6, 4, 15, .3, .3, .1, 32, 1, false);
+	initCoin(1, 4, 15, .3, .3, .1, 32, 1, false);
+	initCoin(.4, 4, 15, .3, .3, .1, 32, 1, false);
 
 	initCylinderPipes(-5, 0, 3);
+	initCylinderPipes(1, 1, 15, 1, 1, 3, 32, 1, false);
 
-	addCoinsRandomly(); // DO COLLISION CHECKS
+	//addCoinsRandomly(); // DO COLLISION CHECKS
 	initSceneDecor(-10, 15);
 	// addDecorRandomly(); // DO COLLISION CHECKS, takes up a lot of mem.
 	initTetrahedron(0, 0, 0);
